404c9af9adc474a56c3a2a02031f9bc1d4237e33
joaoprcf <joaoprcf@ua.pt>
2022-03-15 18:21:44 +0000

    init and sum functions added

--- app.c
+++ app.c
@@ -1,8 +1,12 @@
 
 #include <stdio.h>
 
-void vInit(double *vec, int size) {
+#define VEC_SIZE 20
 
+void vInit(double *vec, int size) {
+    for(int i=1; i<=size; i++) {
+        vec[i-1] = i;
+    }
 }
 
 
@@ -14,12 +18,25 @@ double vAvg(double *vec, int size) {
 
 
 double vSum(double *vec, int size) {
-
-    return 0;
+    double sum = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        sum += vec[i];
+    }
+    
+    return sum;
 }
 
 
 
 int main() {
+    double vector[VEC_SIZE];
+    vInit(vector, VEC_SIZE);
+    for (size_t i = 0; i < VEC_SIZE; i++)
+    {
+        printf("%f ", vector[i]);
+    }
+    printf("\n");
+    printf("%f\n", vSum(vector, VEC_SIZE));
     return 0;
 }
