21e9a708725fa35e7b3545caa8396b6708d0e696
joaoprcf <joaoprcf@ua.pt>
2022-03-15 17:56:32 +0000

    first commit

---
+++ app.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+void vInit(double *vec, int size) {
+
+}
+
+
+
+double vAvg(double *vec, int size) {
+
+    return 0;
+}
+
+
+double vSum(double *vec, int size) {
+
+    return 0;
+}
+
+
+
+int main() {
+    return 0;
+}
